0cdc1dacf593d4cdaebf74617589fc746c03d0e3
Michelyserras <Michelysrrs@gmail.com>
2024-11-04 12:51:13 -0300

    Criando classes para coisas que existem no mundo real

--- Ex1/ConsoleApp1/Program.cs
+++ Ex1/ConsoleApp1/Program.cs
@@ -8,8 +8,20 @@ namespace ConsoleApp1 {
     internal class Program {
         static void Main(string[] args) {
             Carro car1 = new Carro("Volkswagem", "0000", "Bum5225", "azul", 4);
-
+            Moto mot1 = new Moto("Yamaha", "Fazer", "026562", "vermelha", "00000");
             car1.acelerar();
+            car1.desligarMotor();
+            car1.desacelerar();
+            car1.abrirVidro();
+            car1.ligarMotor();
+
+            mot1.acelerar();
+            mot1.desligarMotor();
+            mot1.desacelerar();
+            mot1.cortarDeGiro();
+            mot1.ligarMotor();
+
+
 
         }
     }
---
+++ Ex1/ConsoleApp1/gato.cs
@@ -0,0 +1,46 @@
+﻿
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsoleApp1 {
+    internal class Gato {
+        String raca;
+        String nome;
+        String cor;
+        int idade;
+        int peso;
+
+        public Gato(string raca, string nome, string cor, int idade, int peso) {
+            this.raca = raca;
+            this.nome = nome;
+            this.cor = cor;
+            this.idade = idade;
+            this.peso = peso;
+        }
+        public void Latir() {
+            MessageBox.Show("Miau Miau Miau Miau ...");
+        }
+
+        public void Correr() {
+            MessageBox.Show("correndo...");
+        }
+
+        public void Pular() {
+            MessageBox.Show("Pulando..");
+        }
+
+        public void FazerCarinho() {
+            MessageBox.Show("que carinho bom...");
+        }
+
+        public void Comer() {
+            MessageBox.Show("Comida boa :) ");
+        }
+    }
+}
