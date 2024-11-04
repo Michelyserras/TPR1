3649c127527c127a4f7312630943d34a997dc660
Michelyserras <Michelysrrs@gmail.com>
2024-11-04 14:10:14 -0300

    classe aluno

---
+++ Exercicio_sobre_Classes/Ex2 - Lista de alunos/ConsoleApp1/Aluno.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1 {
+    internal class Aluno {
+        
+
+        public int RA { get; set; }
+        public String Nome { get; set; }
+        public int[] Nota  { get; set; }
+
+        public Aluno() {
+            Nota = new int[4];
+        }
+
+        public Aluno CadastrarAluno() {
+            Aluno aluno = new Aluno();
+
+            Console.WriteLine("Informe o Ra do aluno: ");
+            aluno.RA = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Informe o nome do aluno: ");
+            aluno.Nome = Console.ReadLine();
+
+            for (int i = 0; i < aluno.Nota.Length; i++) {
+                Console.WriteLine($"Informe a {i + 1}a nota");
+                aluno.Nota[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            return aluno;
+        }
+
+        public string situacao(Aluno aluno){
+            double media = 0, soma =0;
+            for(int i=0; i<aluno.Nota.Length; i++) {
+                soma += aluno.Nota[i];
+            }
+            media = soma / 4;
+
+            if()
+        }
+
+    }
+
+}
--- Exercicio_sobre_Classes/Ex2 - Lista de alunos/ConsoleApp1/Program.cs
+++ Exercicio_sobre_Classes/Ex2 - Lista de alunos/ConsoleApp1/Program.cs
@@ -1,12 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace ConsoleApp1 {
     internal class Program {
         static void Main(string[] args) {
+            int op = 0;
+            Aluno novoAluno = new Aluno();
+            List<Aluno> Turma = new List<Aluno>();
+
+            do {
+                op = menu();
+
+                switch (op) {
+                    case 1:
+                        novoAluno = novoAluno.CadastrarAluno();
+                        Turma.Add(novoAluno);
+                        break;
+                    case 2:
+                        foreach(Aluno pessoa in Turma) {
+                            Console.WriteLine($"Ra: {pessoa.RA} \nNome: {pessoa.Nome}");
+                            for(int i=0; i<pessoa.Nota.Length; i++) {
+                                Console.WriteLine($"Nota {i + 1} = {pessoa.Nota[i]}");
+                            }
+                            Console.WriteLine();
+                        }
+                        break;
+                    default:
+                        break;
+                }
+
+            } while (op != 0);
+
+        }
+   
+        public static int menu() {
+            Console.WriteLine("\nInforme uma opção: \n\t [1] Cadastrar Aluno \n\t [2] Exibir Situação \n\t [0] Sair ");
+            int op = Convert.ToInt32(Console.ReadLine());
+            return op;
         }
     }
+
+    
 }
