b07320346c7cfe579045d64bcd05c81eeca2fe4b
Michelyserras <Michelysrrs@gmail.com>
2024-11-04 12:35:11 -0300

    exercicio 1 funcionando

---
+++ Ex1/ConsoleApp1/Carro.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsoleApp1 {
+    internal class Carro {
+        String marca;
+        String chassi;
+        String numPlaca;
+        String cor;
+        int numPortas;
+
+        public Carro(string marca, string chassi, string numPlaca, string cor, int numPortas) {
+            this.marca = marca;
+            this.chassi = chassi;
+            this.numPlaca = numPlaca;
+            this.cor = cor;
+            this.numPortas = numPortas;
+        }
+
+        public void acelerar() {
+            MessageBox.Show("Acelerando vrooommm...");
+        }
+
+        public void desacelerar() {
+            MessageBox.Show("Diminuindo a velocidade...");
+        }
+
+        public void abrirVidro() {
+            MessageBox.Show("Vidro aberto..");
+        }
+
+        public void desligarMotor() {
+            MessageBox.Show("Desligando...");
+        }
+
+        public void ligarMotor() {
+            MessageBox.Show("Motor ligado...");
+        }
+    }
+}
--- Ex1/ConsoleApp1/Program.cs
+++ Ex1/ConsoleApp1/Program.cs
@@ -1,5 +1,16 @@
-﻿internal class Program {
-    private static void Main(string[] args) {
-        Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1 {
+    internal class Program {
+        static void Main(string[] args) {
+            Carro car1 = new Carro("Volkswagem", "0000", "Bum5225", "azul", 4);
+
+            car1.acelerar();
+
+        }
     }
 }
