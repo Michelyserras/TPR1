d38ebda1741e158234a1afb5c9bcb8bbe6d2939e
Michely Serras <137738150+Michelyserras@users.noreply.github.com>
2024-11-27 20:26:44 -0300

    exercício sobre polimorfismo

---
+++ exercicio_sobre_Polimorfismo/Polimorfismo/CestaDeProdutos.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorfismo {
+    internal class CestaDeProdutos {
+        public List<Produto> listaProdutos { get; set; }
+        public double total { get; set; }
+
+        public CestaDeProdutos() {
+            this.listaProdutos = new List<Produto>();
+            this.total = 0;
+        }
+
+        public void AdicionarProduto(Produto p) {
+            listaProdutos.Add(p);
+        }
+
+        public void ExibirItens() {
+            if(listaProdutos.Count > 0) {
+                Console.WriteLine("Lista de Produtos: ");
+                foreach (Produto p in listaProdutos) {
+                    p.exibirProduto();
+                    Console.WriteLine("---------------------------");
+                }
+                Console.WriteLine("\n Total da cesta de compras: {0:c}", CalcularTotal());
+            } else {
+                Console.WriteLine("A Cesta de compras está vazia!");
+            }
+            
+        }
+
+        public void RemoverProduto(Produto p) {
+            if (!listaProdutos.Contains(p)) {
+                Console.WriteLine("Esse produto não existe na lista");
+                return;
+            } else {
+                total -= p.preco;
+                listaProdutos.Remove(p);
+                Console.WriteLine("O produto {0} foi removido com sucesso", p.tipo);
+            }
+
+        }
+     
+
+        public double CalcularTotal() {
+            total = 0;
+            foreach(Produto produto in listaProdutos) {
+                    total += produto.preco;
+                }
+            return total;
+           
+        }
+    }
+}
---
+++ exercicio_sobre_Polimorfismo/Polimorfismo/Produto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorfismo {
+    internal abstract class Produto {
+        public int id { get; set; }
+        public string tipo { get; set; }
+        public string marca { get; set; }
+        public double preco { get; set; }
+
+
+        public Produto(int id, string tipo, string marca,double preco) {
+            this.id = id;
+            this.tipo = tipo;
+            this.marca = marca;
+            this.preco = preco;
+        }
+        public abstract double reajustarPreco();
+
+        public virtual void exibirProduto() {
+            Console.WriteLine("Id: {0}", id);
+            Console.WriteLine("Tipo: {0}", tipo);
+            Console.WriteLine("Marca: {0}", marca);
+            Console.WriteLine("Valor: {0:c}", preco);
+        }
+    }
+}
